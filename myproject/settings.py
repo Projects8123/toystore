66784f6dacb3ec8d9cd55634a72a8e310b4604f2
Projects8123 <awesome123123a@gmail.com>
2026-03-05 03:37:49 +0200

    add cloudinary for media storage

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary', 
     'paypal.standard.ipn',
     'boards',
 ]
@@ -128,3 +129,10 @@ PAYPAL_TEST = os.environ.get("PAYPAL_TEST", "True") == "True"
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 print("CSRF:", CSRF_TRUSTED_ORIGINS)
 print("ALLOWED:", ALLOWED_HOSTS)
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
